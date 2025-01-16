17ca45b6d4dc7c7c6512ca1cee97116fc183f10a
Ridhi Srikanth <rsrikanth@ucsd.edu>
2025-01-15 23:02:26 -0800

    starter code

---
+++ random_quotes/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QuoteGen from './quoteGen';
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteGen />
+    </div>
+  );
+}
+
+export default App;
